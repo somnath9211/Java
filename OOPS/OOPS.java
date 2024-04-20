6bd6c2ae59b40c630a2cda4ade93349533f6574c
Somnath Mandal <somnathmandal6294@gmail.com>
2024-04-20 22:46:53 +0530

    First Class of OOPs

---
+++ OOPS/OOPS.java
@@ -0,0 +1,37 @@
+package OOPS;
+
+public class OOPS {
+
+    public static void main(String[] args) {
+        Pen p1 = new Pen();
+
+        p1.setColor("Blue");
+        System.out.println(p1.color);
+
+        p1.setTip(6);
+        System.out.println(p1.tip);
+    }
+}
+
+class Pen {
+    String color;
+    int tip;
+
+    void setColor(String newColor) {
+        color = newColor;
+    }
+
+    void setTip(int newTip) {
+        tip = newTip;
+    }
+}
+
+class Student {
+    String name;
+    int age;
+    float percentage;
+
+    void calcPercentage(int phy, int math, int chem) {
+        percentage = (phy + math + chem) / 3;
+    }
+}

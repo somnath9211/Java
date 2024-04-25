238d54005b180da98448d6f75eaefa705eb6358d
Somnath Mandal <somnathmandal6294@gmail.com>
2024-04-25 10:54:57 +0530

    OOPS Continue

--- OOPS/OOPS.java
+++ OOPS/OOPS.java
@@ -10,6 +10,31 @@ public class OOPS {
 
         p1.setTip(6);
         System.out.println(p1.tip);
+
+        // Fish shark = new Fish();
+
+        // shark.eat();
+        // shark.breath();
+
+        // Calculator cal = new Calculator();
+
+        // System.out.println(cal.Sum(3, 5));
+        // System.out.println(cal.Sum((float) 3.5, (float) 5.5));
+        // System.out.println(cal.Sum(4, 5, 6));
+
+        // Dear hari = new Dear();
+
+        // hari.eat();
+
+        Horse h = new Horse();
+        h.eat();
+        h.walk();
+
+        Chicken c = new Chicken();
+
+        c.eat();
+        c.walk();
+
     }
 }
 
@@ -33,5 +58,77 @@ class Student {
 
     void calcPercentage(int phy, int math, int chem) {
         percentage = (phy + math + chem) / 3;
+    }
+}
+
+// class Animals {
+// String color;
+
+// void eat() {
+// System.out.println("eats");
+// }
+
+// void breath() {
+// System.out.println("breathes");
+// }
+// }
+
+// class Fish extends Animals {
+// int wins;
+
+// void swime() {
+// System.out.println("swime in water");
+// }
+// }
+
+// Methods OverLoading
+
+// class Calculator {
+// int Sum(int x, int y) {
+// return x + y;
+// }
+
+// float Sum(float x, float y) {
+// return x + y;
+// }
+
+// int Sum(int x, int y, int z) {
+// return x + y + z;
+// }
+// }
+
+// Method OverRinding
+
+// class Animals {
+// void eat() {
+// System.out.println("Eat Anythings");
+// }
+// }
+
+// class Dear extends Animals {
+// void eat() {
+// System.out.println("Eat Grass");
+// }
+// }
+
+// Abstract Classes
+
+abstract class Animal {
+    void eat() {
+        System.out.println("Animals eat");
+    }
+
+    abstract void walk();
+}
+
+class Horse extends Animal {
+    void walk() {
+        System.out.println("Horse walk on 4 legs");
+    }
+}
+
+class Chicken extends Animal {
+    void walk() {
+        System.out.println("Chicken walk on 2 legs");
     }
 }
